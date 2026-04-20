5f1b77915c0ffe8d53a870b074e122035519357f
Agustin Ramos <evilnapsis@sativa.local>
2026-04-20 10:01:38 -0600

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,60 +1,75 @@
-/***
- * BASE DE DATOS DE PROPOSITO GENERAL
- * 
- * 0000   0   0  0  0     0    0   00  000    0000  0   0000 
- * 0      0   0  0  0     0 0  0  0  0 0  0  0      0  0 
- * 000    0   0  0  0     0 0  0  0  0 0  0   0000  0   0000  
- * 0       0 0   0  0     0  0 0  0000 000       0  0      0
- * 0000     0    0  0000  0    0  0  0 0     0000   0  0000
- * https://evilnapsis.com/
- * **/
-create database lbmin;
-use lbmin;
-
-create table user(
+/*
+* Supportix Database
+* @author Evilnapsis
+*/
+create database supportix;
+use supportix; 
+set sql_mode='';
+create table user (
 	id int not null auto_increment primary key,
+	username varchar(50),
 	name varchar(50),
 	lastname varchar(50),
-	username varchar(50),
 	email varchar(255),
 	password varchar(60),
-	image varchar(255),
-	status int default 1,
-	kind int default 1,
+	is_active boolean not null default 1,
+	kind int not null default 1,
 	created_at datetime
 );
 
-/**
-* password: encrypted using sha1(md5("mypassword"))
-* status: 1. active, 2. inactive, 3. other, ...
-* kind: 1. root, 2. other, ...
-**/
+insert into user (username,password,kind,is_active,created_at) value ("admin",sha1(md5("admin")),1,1,NOW());
 
-/* insert user example */
-insert into user (name,lastname, email,password,created_at) value ("Administrator","","admin",sha1(md5("admin")),NOW());
-
-
-create table person(
+create table project (
 	id int not null auto_increment primary key,
-	name varchar(50),
-	lastname varchar(50),
-	email varchar(255),
-	address varchar(255),
-	phone varchar(255),
-	image varchar(255),
-	created_at datetime
+	name varchar(200),
+	description text
+	);
+
+
+create table category (
+	id int not null auto_increment primary key,
+	name varchar(200)
+	);
+
+create table kind (
+	id int not null auto_increment primary key,
+	name varchar(100)
 );
 
-create table setting(
+insert into kind (id,name) values (1,"Ticket"), (2,"Bug"),(3,"Sugerencia"),(4,"Caracteristica");
+
+
+create table status (
 	id int not null auto_increment primary key,
-	name varchar(100) not null unique,
-	label varchar(200) not null,
-	kind int,
-	val text,
-	cfg_id int default 1
+	name varchar(100)
 );
 
-insert into setting(name,label,kind,val) value ("general_main_title","Titulo Principal",1,"LEGOBOX");
-insert into setting(name,label,kind,val) value ("general_version","Version",1,"v4");
-insert into setting(name,label,kind,val) value ("general_author","Autor",1,"Evilnapsis");
-insert into setting(name,label,kind,val) value ("general_year","A&ntilde;o",1,"2022");
+insert into status (id,name) values (1,"Pendiente"), (2,"En Desarrollo"),(3,"Terminado"),(4,"Cancelado");
+
+create table priority (
+	id int not null auto_increment primary key,
+	name varchar(100)
+);
+
+insert into priority (id,name) values  (1,"Alta"),(2,"Media"),(3,"Baja");
+
+create table ticket(
+	id int not null auto_increment primary key,
+	title varchar(100),
+	description text,
+	updated_at datetime,
+	created_at datetime,
+	kind_id int not null,
+	user_id int not null,
+	asigned_id int,
+	project_id int,
+	category_id int,
+	priority_id int not null default 1,
+	foreign key (priority_id) references priority(id),
+	status_id int not null default 1,
+	foreign key (status_id) references status(id),
+	foreign key (user_id) references user(id),
+	foreign key (kind_id) references kind(id),
+	foreign key (category_id) references category(id),
+	foreign key (project_id) references project(id)
+);
